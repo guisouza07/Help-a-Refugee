ae20638dcc78d5a3b4deefab70dd7ac10f5eeef6
Renan Reschke <renanreschke@hotmail.com>
2022-05-15 20:49:37 -0300

    classe abstrata p/ repo

---
+++ back-end/repository/repository.js
@@ -0,0 +1,16 @@
+/**
+ * Abstract Class Animal.
+ *
+ * @class Repository
+ */
+
+class Repository {
+
+    constructor() {
+        if (this.constructor == Repository) {
+            throw new Error("Clase abstrata não pode ser instanciada.")
+        }
+    }
+
+    
+}
