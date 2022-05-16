93b1e5e96a5173b168ad3362470406e072dbe010
Renan Reschke <renanreschke@hotmail.com>
2022-05-15 23:04:55 -0300

    UC

---
+++ back-end/usecases/add-help-offer.js
@@ -0,0 +1,17 @@
+import { Repository } from "../repository/repository";
+
+export class UCAddHelpOffer{
+    constructor(repository){
+        if (!repository instanceof Repository){
+            throw new Error(`Erro UCAddHelpOffer. Tipo errado para o parâmetro repository (${typeof(repository)})`)
+        }
+        this._repository = repository
+    }
+
+    addHelpOffer(helpOffer){
+        if (!helpOffer instanceof HelpOffer){
+            throw new Error(`Erro método Volatile.addHelpOffer. Tipo de parâmetro errado (${typeof(helpOffer)})`)
+        }
+        this._repository.addHelpOffer(helpOffer)
+    }
+}
