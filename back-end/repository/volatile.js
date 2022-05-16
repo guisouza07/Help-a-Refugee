19403aa2f2dea0f3886cd0f13c0883a9848a2752
Renan Reschke <renanreschke@hotmail.com>
2022-05-15 23:04:20 -0300

    alt

--- back-end/repository/volatile.js
+++ back-end/repository/volatile.js
@@ -5,12 +5,18 @@ import { Interest } from '../models/interest'
 export class Volatile extends Repository{
     constructor(){
         this._helpOffers = {}
+        this._helpOffersMaxId = 0
         this._interests = {}
+        this._interestsMaxId = 0
     }
 
     addHelpOffer(helpOffer){
         if (!helpOffer instanceof HelpOffer){
             throw new Error(`Erro método Volatile.addHelpOffer. Tipo de parâmetro errado (${typeof(helpOffer)})`)
+        }
+        if (helpOffer.id <= 0){
+            helpOffer.id = this._helpOffersMaxId + 1
+            this._helpOffersMaxId += 1
         }
         this._helpOffers.push(helpOffer)
     }
@@ -19,6 +25,12 @@ export class Volatile extends Repository{
         if (!interest instanceof Interest){
             throw new Error(`Erro método Volatile.addInterest. Tipo de parâmetro errado (${typeof(interest)})`)
         }
+
+        if (interest.id <= 0){
+            interest.id = this._interestMaxId + 1
+            this._interestMaxId += 1
+        }
+
         this._interests.push(interest)
     }
 }
