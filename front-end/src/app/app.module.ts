bc522bcab47ecb0d27530bab2fdb30fc6d966a7d
Felipe Villani <felipevillani.2000@hotmail.com>
2022-05-19 00:30:58 -0300

    Add imports

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,9 +5,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CadastrarUsuarioComponent } from './cadastrar-usuario/cadastrar-usuario.component';
 
+import { FormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatStepperModule} from '@angular/material/stepper';
 
 
@@ -19,9 +24,14 @@ import {MatStepperModule} from '@angular/material/stepper';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatInputModule,
     MatIconModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     MatStepperModule
   ],
   providers: [],
