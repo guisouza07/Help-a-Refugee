de7c097d8e57c16235d79de199c2d62ce5c37137
Felipe Villani <felipevillani.2000@hotmail.com>
2022-05-25 22:56:41 -0300

    add ReactiveForms

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatExpansionModule} from '@angular/material/expansion';
@@ -40,7 +40,8 @@ import { AjudaComponent } from './ajuda/ajuda.component';
     MatRadioModule,
     MatSelectModule,
     MatSlideToggleModule,
-    MatStepperModule
+    MatStepperModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
