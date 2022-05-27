4a5559ea53002107a488b0277a775c822fdd35dc
Renan Reschke <renanreschke@hotmail.com>
2022-05-27 19:34:48 -0300

    Mudança nos models

--- back-end/models/help-offer.js
+++ back-end/models/help-offer.js
@@ -2,13 +2,16 @@ import { DateTime } from 'Luxon'
 import { HelpTypeEnum } from '../enums/help-type.js';
 
 export class HelpOffer{
-    constructor(id, announcerName, announceDate, location, helpType, description){
+    constructor(id, announcerName, announcerSirName, announceDate, location, helpType, description, email, sex, announcerBirth){
         //Validações de parâmetros
         if (!id instanceof Number){
             throw new Error(`Erro construtor HelpOffer. Tipo incorreto para o parâmetro id (${typeof(id)})`);
         }
         if (!announcerName instanceof String){
-            throw new Error(`Erro construtor HelpOffer. Tipo incorreto para o parâmetro announceName (${typeof(announceName)})`);
+            throw new Error(`Erro construtor HelpOffer. Tipo incorreto para o parâmetro announcerName (${typeof(announcerName)})`);
+        }
+        if (!announcerSirName instanceof String){
+            throw new Error(`Erro construtor HelpOffer. Tipo incorreto para o parâmetro announcerSirName (${typeof(announcerSirName)})`);
         }
         if (!announceDate instanceof DateTime){
             throw new Error(`Erro construtor HelpOffer. Tipo incorreto para o parâmetro announceDate (${typeof(announceDate)})`);
@@ -22,13 +25,26 @@ export class HelpOffer{
         if (!description instanceof String){
             throw new Error(`Erro construtor HelpOffer. Tipo incorreto para o parâmetro description (${typeof(description)})`);
         }
+        if (!email instanceof String){
+            throw new Error(`Erro construtor HelpOffer. Tipo incorreto para o parâmetro email (${typeof(email)})`);
+        }
+        if (!sex instanceof String){
+            throw new Error(`Erro construtor HelpOffer. Tipo incorreto para o parâmetro sexo (${typeof(sex)})`);
+        }
+        if (!announcerBirth instanceof DateTime){
+            throw new Error(`Erro construtor HelpOffer. Tipo incorreto para o parâmetro announcerBirth (${typeof(announcerBirth)})`);
+        }
 
         this._id = id
         this._announcerName = announcerName
+        this._announcerSirName = announcerSirName
         this._announceDate = announceDate
         this._location = location
         this._helpType = helpType
         this._description = description
+        this._email = email
+        this._sex = sex
+        this._announcerBirth = announcerBirth
     }
 
     get id(){
@@ -37,6 +53,10 @@ export class HelpOffer{
 
     get announcerName(){
         return this._announcerName
+    }
+
+    get announcerSirName(){
+        return this._announcerSirName
     }
 
     get announceDate(){
@@ -55,12 +75,28 @@ export class HelpOffer{
         return this._description
     }
 
+    get email(){
+        return this._email
+    }
+
+    get sex(){
+        return this._sex
+    }
+
+    get announcerBirth(){
+        return this._announcerBirth
+    }
+
     set id(id){
         this._id = id
     }
 
     set announcerName(name){
         this._announcerName = name
+    }
+
+    set announcerSirName(name){
+        this._announcerSirName = name
     }
 
     set announceDate(date){
@@ -77,5 +113,17 @@ export class HelpOffer{
 
     set description(description){
         this._description = description
+    }
+
+    set email(email){
+        this._email = email
+    }
+
+    set sex(sex){
+        this._sex = sex
+    }
+
+    set announcerBirth(date){
+        this._announcerBirth = date
     }
 }
--- back-end/models/interest.js
+++ back-end/models/interest.js
@@ -1,13 +1,16 @@
 import { DateTime } from 'Luxon'
 
 export class Interest{
-    constructor(id, announcerName, announceDate, email, idHelpOffer){
+    constructor(id, announcerName, announcerSirName, announceDate, email, idHelpOffer, sex, announcerBirth){
         //Validações de parâmetros
         if (!id instanceof Number){
             throw new Error(`Erro construtor Interest. Tipo incorreto para o parâmetro id (${typeof(id)})`);
         }
         if (!announcerName instanceof String){
             throw new Error(`Erro construtor Interest. Tipo incorreto para o parâmetro announceName (${typeof(announceName)})`);
+        }
+        if (!announcerSirName instanceof String){
+            throw new Error(`Erro construtor HelpOffer. Tipo incorreto para o parâmetro announcerSirName (${typeof(announcerSirName)})`);
         }
         if (!announceDate instanceof DateTime){
             throw new Error(`Erro construtor Interest. Tipo incorreto para o parâmetro announceDate (${typeof(announceDate)})`);
@@ -18,12 +21,21 @@ export class Interest{
         if (!idHelpOffer instanceof Number){
             throw new Error(`Erro construtor Interest. Tipo incorreto para o parâmetro idHelpOffer (${typeof(idHelpOffer)})`);
         }
+        if (!sex instanceof String){
+            throw new Error(`Erro construtor HelpOffer. Tipo incorreto para o parâmetro sexo (${typeof(sex)})`);
+        }
+        if (!announcerBirth instanceof DateTime){
+            throw new Error(`Erro construtor HelpOffer. Tipo incorreto para o parâmetro announcerBirth (${typeof(announcerBirth)})`);
+        }
 
         this._id = id
         this._announcerName = announcerName
+        this._announcerSirName = announcerSirName
         this._announceDate = announceDate
         this._email = email
         this._idHelpOffer = idHelpOffer
+        this._sex = sex
+        this._announcerBirth = announcerBirth
     }
 
     get id(){
@@ -32,6 +44,10 @@ export class Interest{
 
     get announcerName(){
         return this._announcerName
+    }
+
+    get announcerSirName(){
+        return this._announcerSirName
     }
 
     get announceDate(){
@@ -46,13 +62,24 @@ export class Interest{
         return this._idHelpOffer
     }
 
+    get sex(){
+        return this._sex
+    }
+
+    get announcerBirth(){
+        return this._announcerBirth
+    }
+
     set id(id){
         this._id = id
     }
 
-
     set announcerName(name){
         this._announcerName = name
+    }
+
+    set announcerSirName(name){
+        this._announcerSirName = name
     }
 
     set announceDate(date){
@@ -61,5 +88,13 @@ export class Interest{
 
     set email(email){
         this._email = email
+    }
+
+    set sex(sex){
+        this._sex = sex
+    }
+
+    set announcerBirth(date){
+        this._announcerBirth = date
     }
 }
