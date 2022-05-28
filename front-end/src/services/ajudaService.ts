aa18bd1a7f3fc9a28754be16599a6379598feb0c
Felipe Villani <felipevillani.2000@hotmail.com>
2022-05-28 10:59:13 -0300

    update

--- front-end/src/services/ajudaService.ts
+++ front-end/src/services/ajudaService.ts
@@ -1,6 +1,9 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Ajuda } from "src/app/models/ajuda";
 import { Injectable } from '@angular/core';
+import { environment } from "src/environments/environment";
+import { Observable } from "rxjs";
+import { ConditionalExpr } from "@angular/compiler";
 
 @Injectable({
   providedIn: 'root',
@@ -8,16 +11,37 @@ import { Injectable } from '@angular/core';
 
 export class AjudaService{
 
-    private readonly url = '';
+    private readonly apiURL = environment.apiURL;
+    private readonly routePost = '/help-offer';
+    private readonly routeGet = '/help-offers';
 
-    constructor(private http: HttpClient){}
+    private headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:4200',
+        'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+    });
+
+    private options = { headers: this.headers };
+
+    constructor(private http: HttpClient){
+    }
 
     get(){
-        return this.http.get(this.url)
+        return this.http.get(this.apiURL + this.routeGet)
     }
 
     create(ajuda: Ajuda){
-        return this.http.post(this.url, ajuda)
+        console.log(this.options)
+        return this.http.post(this.apiURL + this.routePost, ajuda, this.options)
+            .subscribe(
+                (data) => {
+
+                },
+                (error) => {
+                    console.log(error);
+                }
+            )
     }
 
 }
