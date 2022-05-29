c4fb3721ae1fc3f66e3a48670457759cdd92912d
Felipe Villani <felipevillani.2000@hotmail.com>
2022-05-29 15:49:03 -0300

    create ajuda request

---
+++ front-end/src/app/models/ajudaRequest.ts
@@ -0,0 +1,14 @@
+export class AjudaRequest{
+
+    constructor(){}
+
+    announcerName: string
+    announcerSirName: string
+    email: string
+    location: string
+    helpType: string
+    description: string
+    sex: string
+    announcerBirth: string
+    
+}
